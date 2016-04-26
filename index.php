00fc9ce2a9234b840e1af9b9545868ffb8dfdb71
Kai Garrott <garrottkai@gmail.com>
2016-04-26 10:11:13 -0600

    edit css

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+
+		<link rel="stylesheet" href="css/style.css" />
+
+		<title>CSS practice</title>
+	</head>
+	<body>
+		<header>
+			<div class="container">
+				<a href="#">CSS practice</a>
+				<p>This is a paragraph containing lots of fascinating and informative textual content.</p>
+				<ul>
+					<li>This is an item</li>
+					<li>This is another item</li>
+					</ul>
+				<p>'Nother paragraph for you to read</p>
+				<p>Paragraph number three</p>
+			</div>
+		</header>
+		<main>
+			<div class="container">
+				<p>Oh my god, this paragraph is the greatest paragraph you've ever read in the history of test page filler text!!</p>
+			</div>
+		</main>
+
+	</body>
+</html>
