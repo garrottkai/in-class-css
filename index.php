dc948a07d2325257a5e27aa0035af400efe866a8
Kai Garrott <garrottkai@gmail.com>
2016-04-26 10:40:09 -0600

    css edits

--- index.php
+++ index.php
@@ -19,10 +19,6 @@
 			<div class="container">
 				<p>Oh my god, this paragraph is the greatest paragraph you've ever read in the history of test page filler text!!</p>
 				<p id="width-50">This is a paragraph containing lots of fascinating and informative textual content.</p>
-				<ul>
-					<li>This is an item</li>
-					<li>This is another item</li>
-				</ul>
 				<p>'Nother paragraph for you to read</p>
 				<p>Paragraph number three</p>
 			</div>
