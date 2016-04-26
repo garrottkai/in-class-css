4ec3707881a7efab9848b9a24d866638bbf30e06
Kai Garrott <garrottkai@gmail.com>
2016-04-26 10:49:53 -0600

    html edits

--- index.php
+++ index.php
@@ -17,9 +17,9 @@
 		</header>
 		<main>
 			<div class="container">
-				<p>Oh my god, this paragraph is the greatest paragraph you've ever read in the history of test page filler text!!</p>
+				<p>Oh my god, this paragraph is the greatest paragraph you've ever read in the history of test page filler text!!</p><br>
 				<p id="width-50">This is a paragraph containing lots of fascinating and informative textual content.</p>
-				<p>'Nother paragraph for you to read</p>
+				<p>'Nother paragraph for you to read</p><br>
 				<p>Paragraph number three</p>
 			</div>
 		</main>
