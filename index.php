ea403ac7c62239a756068a35d1282d9d030ae8e7
Kai Garrott <garrottkai@gmail.com>
2016-04-26 10:23:25 -0600

    even more css edits

--- index.php
+++ index.php
@@ -10,19 +10,21 @@
 	<body>
 		<header>
 			<div class="container">
-				<a href="#">CSS practice</a>
-				<p id="width-50">This is a paragraph containing lots of fascinating and informative textual content.</p>
-				<ul>
-					<li>This is an item</li>
-					<li>This is another item</li>
-					</ul>
-				<p>'Nother paragraph for you to read</p>
-				<p>Paragraph number three</p>
+				<h1>CSS Practice</h1>
+				<p>This is a test page</p>
+
 			</div>
 		</header>
 		<main>
 			<div class="container">
 				<p>Oh my god, this paragraph is the greatest paragraph you've ever read in the history of test page filler text!!</p>
+				<p id="width-50">This is a paragraph containing lots of fascinating and informative textual content.</p>
+				<ul>
+					<li>This is an item</li>
+					<li>This is another item</li>
+				</ul>
+				<p>'Nother paragraph for you to read</p>
+				<p>Paragraph number three</p>
 			</div>
 		</main>
 
